b37bd1bcc017ed5ad104c847b8ebd01924afd293
tyamap <tom333my@outlook.com>
2020-09-22 14:13:03 +0900

    add 年月pickerボタン

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="navigation-bar">
     <V-toolbar height="50px" color="cyan darken-3" dark flat>
       <V-app-bar-nav-icon></V-app-bar-nav-icon>
       <V-btn icon small @click="setLastMonth"
@@ -8,7 +8,21 @@
       <V-btn icon small @click="setNextMonth"
         ><V-icon>mdi-chevron-right</V-icon></V-btn
       >
-      <V-toolbar-title>{{ year }} / {{ month }}</V-toolbar-title>
+      <V-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <V-toolbar-title v-bind="attrs" v-on="on" @click="setPicker">
+            {{ year }} / {{ month }}
+          </V-toolbar-title>
+        </template>
+        <V-date-picker
+          v-model="picker"
+          elevation="15"
+          type="month"
+          color="pink lighten-3"
+          @input="setYearMonth"
+          reactive
+        ></V-date-picker>
+      </V-menu>
       <V-spacer></V-spacer>
       <V-btn outlined small @click="setCurrentYearAndMonth">today</V-btn>
     </V-toolbar>
@@ -19,11 +33,31 @@
 import { mapGetters, mapActions } from "vuex";
 
 export default {
-  computed: mapGetters(["year", "month"]),
-  methods: mapActions([
-    "setLastMonth",
-    "setNextMonth",
-    "setCurrentYearAndMonth",
-  ]),
+  data() {
+    return {
+      picker: null
+    };
+  },
+  computed: {
+    ...mapGetters(["year", "month", "today"]),
+  },
+  methods:{
+    ...mapActions(["setLastMonth", "setNextMonth", "setCurrentYearAndMonth"]),
+    setYearMonth: function(){
+      const year = Number(this.picker.slice(0,4));
+      const month = Number(this.picker.slice(-2));
+      this.$store.dispatch("setYear", year);
+      this.$store.dispatch("setMonth", month);
+    },
+    setPicker: function() {
+      this.picker = this.year + "-" + ("00" + this.month).slice(-2)
+    }
+  }
 };
 </script>
+
+<style>
+#navigation-bar {
+  user-select: none;
+}
+</style>
